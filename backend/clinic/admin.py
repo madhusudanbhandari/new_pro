7acf57c2695a51f1310ab82ae559e646186e07ab
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-06-03 14:40:36 +0545

    created models and admin register

--- backend/clinic/admin.py
+++ backend/clinic/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import User,DoctorProfile,PatientProfile,AdminProfile
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','email','role']
+
+@admin.register(DoctorProfile)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display=['doctor_name','doctor_email','specialization']
+
+    def doctor_name(self,obj):
+        return obj.user.name
+    
+    def doctor_email(self,obj):
+        return obj.user.email
+    
+@admin.register(PatientProfile)
+class PatientAdmin(admin.ModelAdmin):
+    list_display=['patient_name','patient_email','age']
+
+    def patient_name(self,obj):
+        return obj.user.name
+    
+    def patient_email(self,obj):
+        return obj.user.email
+    
+    
+@admin.register(AdminProfile)
+class Admin(admin.ModelAdmin):
+    list_display=['admin_name','admin_email','department']
+
+    def admin_name(self,obj):
+        return obj.user.name
+    
+    def admin_email(self,obj):
+        return obj.user.email
+    
+    
--- backend/clinic/models.py
+++ backend/clinic/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES=(
+        ('admin','Admin'),
+        ('doctor','Doctor'),
+        ('patient','Patient'),
+    )
+    
+    role=models.CharField(max_length=10,choices=ROLE_CHOICES)
+
+
+class DoctorProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
+    specialization = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50)
+    experience_years = models.IntegerField()
+    hospital = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.name
+
+class PatientProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    blood_group = models.CharField(max_length=5)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.user.name
+
+class AdminProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
+    department=models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.name
